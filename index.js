a4a76a186f129d5469b85e643c5aff088e45ba5d
wildcodeflower <codyglane@gmail.com>
2025-10-29 10:16:33 -0700

    button text size not responsive

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function openMenu() {
-document.body.classlist += " menu--open"
+document.body.classList += " menu--open"
 }
 
 function closeMenu() {
-document.body.classlist.remove('menu--open')
+document.body.classList.remove('menu--open')
 }
